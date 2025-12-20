bc3183d95115f495ca3b0c257367cbd982cf7098
Lukas <r.yusifli@yahoo.com>
2025-12-20 21:03:19 +0100

    Updated docusaurus.config.ts

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://dev-lukas0.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/Sound-Manager/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
